3a13e8dc79098924fc8d80326544749e99236708
Yasser <yasserhhh161@gmail.com>
2024-12-22 22:33:04 +0200

    home slider

--- src/app/core/components/user/home-slider/home-slider.component.ts
+++ src/app/core/components/user/home-slider/home-slider.component.ts
@@ -1,18 +1,23 @@
-import { isPlatformBrowser, NgClass, NgFor } from '@angular/common';
+import { isPlatformBrowser, NgClass } from '@angular/common';
 import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-home-slider',
   standalone: true,
-  imports: [NgFor, NgClass],
+  imports: [NgClass],
   templateUrl: './home-slider.component.html',
   styleUrl: './home-slider.component.css'
 })
 export class HomeSliderComponent implements OnInit, OnDestroy {
   slides = [
     { imgUrl: 'https://images.squarespace-cdn.com/content/v1/66a112f13bc71a7edaecb5aa/a321d1af-b14b-4237-ae7d-48bd8971a7a1/2024-07_Messi-Family-Hero_01.jpg?format=2500w' },
-    { imgUrl: 'https://images.squarespace-cdn.com/content/v1/66a112f13bc71a7edaecb5aa/e05643a8-5234-46f3-820c-dd7adb6fceb2/2024-07_Messi-Family_01.jpg?format=2500w' }
+    { imgUrl: 'https://images.squarespace-cdn.com/content/v1/66a112f13bc71a7edaecb5aa/e05643a8-5234-46f3-820c-dd7adb6fceb2/2024-07_Messi-Family_01.jpg?format=2500w' },
     ];
+    moblileslides = [
+      {imgUrl: 'https://shopzedzee.com/cdn/shop/files/BOO06198.jpg?v=1730245848&width=1500'},
+      { imgUrl: 'https://shopzedzee.com/cdn/shop/files/BOO06021.jpg?v=1730245848&width=1500'}
+    ];
+    activearray = [{imgUrl:''}];
 
   currentSlide = 0;
   autoSlideInterval: any;
@@ -23,6 +28,8 @@ export class HomeSliderComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.updateArray();
+    this.listenToMediaQuery();
     if (this.isBrowser) {
       this.autoSlide();
     }
@@ -47,4 +54,15 @@ export class HomeSliderComponent implements OnInit, OnDestroy {
       clearInterval(this.autoSlideInterval);
     }
   }
+  updateArray(): void {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      this.activearray = this.moblileslides;
+    } else {
+      this.activearray = this.slides;
+    }
+  }
+listenToMediaQuery(): void {
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+  mediaQuery.addEventListener('change', () => this.updateArray());
+}
 }
