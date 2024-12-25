16df784b53f895cdba731e9156908872c32ec3f6
Yasser <yasserhhh161@gmail.com>
2024-12-25 23:54:53 +0200

    redirect link

--- src/app/core/components/user/check-out/check-out.component.ts
+++ src/app/core/components/user/check-out/check-out.component.ts
@@ -123,7 +123,7 @@ onSubmit(event : any , checkoutform:NgForm){
             localStorage.removeItem('Total-info');
             this._cartservice.UpdateHeaderValue(true);
             var webhookendpoint = 'https://orca-app-sw4g7.ondigitalocean.app/api/WebHook/CompletePayment';
-            this.source = `https://checkout.kashier.io/?merchantId=MID-29963-501&orderId=${result.orderAdditionalData.id}&amount=${result.orderAdditionalData.totalPrice}&currency=EGP&hash=${result.orderAdditionalData.hash}&mode=test&metaData={"metaData":"myData"}&merchantRedirect=http://localhost:4200/home&serverWebhook=${webhookendpoint}&allowedMethods=card,wallet&failureRedirect=false&redirectMethod=get&brandColor=%2300bcbc&display=en&type=external`;
+            this.source = `https://checkout.kashier.io/?merchantId=MID-29963-501&orderId=${result.orderAdditionalData.id}&amount=${result.orderAdditionalData.totalPrice}&currency=EGP&hash=${result.orderAdditionalData.hash}&mode=test&metaData={"metaData":"myData"}&merchantRedirect=https://verdant-granita-c77051.netlify.app/home&serverWebhook=${webhookendpoint}&allowedMethods=card,wallet&failureRedirect=false&redirectMethod=get&brandColor=%2300bcbc&display=en&type=external`;
             this.document.location.href = this.source;
             this.isLoading = false;
           }
