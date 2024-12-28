e77cbb1f8d1f8fdab4ebf803aefe82f1332df760
Yasser <yasserhhh161@gmail.com>
2024-12-28 18:34:21 +0200

    webhook

--- src/app/core/components/user/check-out/check-out.component.ts
+++ src/app/core/components/user/check-out/check-out.component.ts
@@ -122,7 +122,7 @@ onSubmit(event : any , checkoutform:NgForm){
             localStorage.removeItem('cart');
             localStorage.removeItem('Total-info');
             this._cartservice.UpdateHeaderValue(true);
-            var webhookendpoint = 'https://orca-app-sw4g7.ondigitalocean.app/api/WebHook/CompletePayment';
+            var webhookendpoint = 'https://eleve.runasp.net/api/WebHook/CompletePayment';
             this.source = `https://checkout.kashier.io/?merchantId=MID-29963-501&orderId=${result.orderAdditionalData.id}&amount=${result.orderAdditionalData.totalPrice}&currency=EGP&hash=${result.orderAdditionalData.hash}&mode=test&metaData={"metaData":"myData"}&merchantRedirect=https://verdant-granita-c77051.netlify.app/home&serverWebhook=${webhookendpoint}&allowedMethods=card,wallet&failureRedirect=false&redirectMethod=get&brandColor=%2300bcbc&display=en&type=external`;
             this.document.location.href = this.source;
             this.isLoading = false;
