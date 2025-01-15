edd656682495053b37432e743513a8884e1ae461
Yasser <yasserhhh161@gmail.com>
2025-01-15 20:13:46 +0200

    gg

--- src/app/core/interfaces/IProductDetails.ts
+++ src/app/core/interfaces/IProductDetails.ts
@@ -4,6 +4,8 @@ import { ISizeColorQuantity } from "./ISizeColorQuantity";
 export interface IProductDetails {
     id: number;
     name: string;
+    summary: string;
+    fullDescription: string;
     priceBeforeDiscount: number;
     priceAfterDiscount: number;
     discount: number;
