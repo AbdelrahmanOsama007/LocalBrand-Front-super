b9d67bc5b2c096d8133b1f5f03802e221aa292d1
Yasser <yasserhhh161@gmail.com>
2025-02-08 00:30:13 +0200

    jj

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
-import 'zone.js';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
 
 
 bootstrapApplication(AppComponent, appConfig)
