b3b8271028e1c74e9e9822af26239cb173b6278f
Yasser <yasserhhh161@gmail.com>
2025-02-07 18:45:13 +0200

    vamos

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import 'zone.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
