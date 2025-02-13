9bed05180e32723b86c64ae8fb857b72a3de690a
Yasser <yasserhhh161@gmail.com>
2025-02-13 23:48:03 +0200

    الله وكيل احنا عايشين مع بهايم

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
-import 'whatwg-fetch';
+// import 'whatwg-fetch';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
